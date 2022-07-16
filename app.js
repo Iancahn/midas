e8fb027491984b856785eae9263067568ce343d5
Ian Cahn <iancahn@live.com>
2022-07-16 23:44:25 +0200

    Navbar Done
    Navbar + Mobile Responsiveness + MM_Button Animation

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-// console.log("Hello Ian");
+console.log("Hello World");
 
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+menu.addEventListener('click', function () {
+    menu.classList.toggle('is-active');
+    menu.classList.toggle('active');
+});
